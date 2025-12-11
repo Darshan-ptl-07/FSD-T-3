a427c60cd1334ae759d94a9fc0bb249ef4aff80f
Darshan-ptl-07 <darshangothi340@gmail.com>
2025-12-11 10:47:36 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    print("sum:",a+b)
+def sub(a,b):
+    print("sub:",a-b)
+def mul(a,b):
+    print("mul:",a*b)
+def div(a,b):
+    print("div:",a/b)
+def power(a,b):
+    print("power:",a**b)
+def module(a,b):
+    print("module:",a%b)
+def floor(a,b):
+    print("floor div:",a//b)
